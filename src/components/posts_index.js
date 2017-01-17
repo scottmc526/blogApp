a7038957ab6215f1c215ed8ae4c4e4f177947961
Scott Christensen <scottmc526@gmail.com>
2017-01-17 14:03:44 -0700

    add post button

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
   componentWillMount() {
@@ -9,7 +10,12 @@ class PostsIndex extends Component {
 
   render() {
     return (
-      <div>list of posts</div>
+      <div>
+        <div className='text-xs-right'>
+          <Link to='/posts/new' className='btn btn-primary'>Add a Post</Link>
+        </div>
+        list of Posts
+      </div>
     )
   }
 }
