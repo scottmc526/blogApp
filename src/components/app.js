cf8cb9ee2d8555d1cc02d99eeff2026da509f824
Scott Christensen <scottmc526@gmail.com>
2017-01-16 22:41:41 -0700

    index route

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Posts from './posts_index';
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        {this.props.children}
+      </div>
     );
   }
 }
