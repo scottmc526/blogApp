370ae59affcd0c2f528cdf1462be540d34aad63e
Scott Christensen <scottmc526@gmail.com>
2017-01-17 20:40:24 -0700

    new post form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { Link } from 'react-router'
 
 class PostsNew extends Component {
   render () {
     return (
-      <div>Create form</div>
+      <form>
+        <h3>Create a New Post</h3>
+        <div className='form-group'>
+          <label>Title</label>
+          <input className='form-control' type='text' placeholder='Title' />
+        </div>
+        <div className='form-group'>
+          <label>Categories</label>
+          <input className='form-control' type='text' placeholder='Categories' />
+        </div>
+        <div className='form-group'>
+          <label>Content</label>
+          <textarea className='form-control'  placeholder='Describe' />
+        </div>
+        <button type='submit' className='btn btn-primary'>Submit</button>
+        <Link to='/' className='btn btn-danger'>Cancel</Link>
+      </form>
+
+
     )
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+})(PostsNew);
